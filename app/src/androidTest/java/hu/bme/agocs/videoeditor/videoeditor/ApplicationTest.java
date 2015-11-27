b11aaee3bb3c54951911798abbd0a26f33ae8018
Agócs Tamás <agta91@gmail.com>
2015-11-27 19:46:12 +0100

    Initial commit

---
+++ app/src/androidTest/java/hu/bme/agocs/videoeditor/videoeditor/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hu.bme.agocs.videoeditor.videoeditor;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
